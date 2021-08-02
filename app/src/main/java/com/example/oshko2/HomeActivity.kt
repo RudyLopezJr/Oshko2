0978955e9dcb3da56ac93cf9e3ae6ae161b54ad2
RudyLopezJr <67517652+RudyLopezJr@users.noreply.github.com>
2021-08-01 23:03:27 -0500

    adding funcionality to menu

--- app/src/main/java/com/example/oshko2/HomeActivity.kt
+++ app/src/main/java/com/example/oshko2/HomeActivity.kt
@@ -3,8 +3,15 @@ package com.example.oshko2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.example.oshko2.fragments.HomeFragment
+import com.example.oshko2.fragments.ProfileFragment
+import com.example.oshko2.fragments.ShoppingCartFragment
+import com.example.oshko2.fragments.StarFragment
+import kotlinx.android.synthetic.main.activity_home.*
 
 const val USER_NAME = "com.example.oshko2.USER_NAME"
 
@@ -13,12 +20,35 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var boton: Button
     private lateinit var input: EditText
+    private val homeFragment = HomeFragment()
+    private val profileFragment = ProfileFragment()
+    private val shoppingCartFragment = ShoppingCartFragment()
+    private val starFragment = StarFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        replaceFragment(homeFragment)
 
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> replaceFragment(homeFragment)
+                R.id.ic_profile -> replaceFragment(profileFragment)
+                R.id.ic_shopping_cart -> replaceFragment(shoppingCartFragment)
+                R.id.ic_star -> replaceFragment(starFragment)
+            }
+            true 
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
 
