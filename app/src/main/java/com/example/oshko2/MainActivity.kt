e6e237b953552a4813a63562da5eb24e317cf098
Andy <74468655+KUSHIEKUSHIE@users.noreply.github.com>
2021-08-04 19:37:18 -0500

    Adding mip map and layout profile

--- app/src/main/java/com/example/oshko2/MainActivity.kt
+++ app/src/main/java/com/example/oshko2/MainActivity.kt
@@ -8,13 +8,22 @@ import android.os.Handler
 import androidx.core.os.HandlerCompat.postDelayed
 
 
-class MainActivity : AppCompatActivity() {
+class
+MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        open_btn=findViewById(R.id.open_btn)
+        text=findViewById(R.id.text)
+        open_btn.setOnClickListener {
+            text.text = getString(R.string.button_clicked)
+            text.visibility = View.INVISIBLE
+            open_btn.isEnabled = false
+        }
+
 
         supportActionBar?.hide()
 
